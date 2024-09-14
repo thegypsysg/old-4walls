14cb602f25d6b6018a536f8eed7c932447c3cc87
Rizjami Putera <rizjamiputera@gmail.com>
2024-09-14 20:51:04 +0700

    add fetured-move

--- src/views/city-properties/indexView.vue
+++ src/views/city-properties/indexView.vue
@@ -10,6 +10,7 @@
     <v-container class="mx-auto px-4 medium:px-16" style="max-width: 1200px">
       <Slider />
       <FeaturedProject />
+      <FeaturedMove />
     </v-container>
     <Footer />
   </div>
@@ -19,6 +20,7 @@
 import Banner from './partials/banner'
 import Slider from './partials/slider'
 import FeaturedProject from './partials/featured-project'
+import FeaturedMove from './partials/featured-move'
 
 </script>
 
