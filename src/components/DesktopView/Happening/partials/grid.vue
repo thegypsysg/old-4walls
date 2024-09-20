31efde707eb019c12b5789cbaae65463b1778164
Rizjami Putera <rizjamiputera@gmail.com>
2024-09-21 01:51:13 +0700

    Update happening

---
+++ src/components/DesktopView/Happening/partials/grid.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-hover v-slot:default="{ isHovering, props }">
+    <v-responsive v-if="isSquare" aspect-ratio="1"
+      :style="`height: 100%; width: 100%; background-image:url('${image}'); background-size: cover;`"
+      class="trending fade-in-up rounded">
+      <div v-bind="props"
+        :style="[isHovering ? 'background-color: rgba(0, 0, 0, 0.0)' : 'background-color: rgba(0, 0, 0, 0.3)', 'transition: background-color 0.3s ease']"
+        class="fill-height d-flex flex-column align-center justify-between px-8 py-8 text-white font-weight-light">
+        <div class="text-center font-weight-bold">
+          <div class="text-h4 font-weight-bold text-white">{{ title }}</div>
+          <div>24 Properties</div>
+        </div>
+        <div class="text-center mt-auto mb-auto">
+          <v-btn class="ma-2" color="pink darken-1" dark rounded>View Options</v-btn>
+        </div>
+      </div>
+    </v-responsive>
+    <v-responsive v-else :style="`height: 100%; width: 100%; background-image:url('${image}'); background-size: cover;`"
+      class="trending fade-in-up rounded">
+      <div v-bind="props"
+        :style="[isHovering ? 'background-color: rgba(0, 0, 0, 0.0)' : 'background-color: rgba(0, 0, 0, 0.3)', 'transition: background-color 0.3s ease']"
+        class="fill-height d-flex flex-column align-center justify-between px-8 py-8 text-white font-weight-light">
+        <div class="text-center font-weight-bold">
+          <div class="text-h4 font-weight-bold text-white">{{ title }}</div>
+          <div>24 Properties</div>
+        </div>
+        <div class="text-center mt-auto mb-auto">
+          <v-btn class="ma-2" color="pink darken-1" dark rounded>View Options</v-btn>
+        </div>
+      </div>
+    </v-responsive>
+  </v-hover>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  image: string
+  isSquare: boolean
+}>()
+
+</script>
