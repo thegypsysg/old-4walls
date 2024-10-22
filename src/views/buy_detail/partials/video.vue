b71d85c3801413547262c1dcb7556dc19537f4c4
aijprasetyo471 <ajiprsty4713@gmail.com>
2024-10-22 22:18:45 +0700

    implement api 8 task 317

--- src/views/buy_detail/partials/video.vue
+++ src/views/buy_detail/partials/video.vue
@@ -2,18 +2,24 @@
   <v-container class="pa-9 bg-white">
     <div>Video</div>
 
-    <hr class="my-6">
+    <hr class="my-6" />
 
     <v-responsive aspect-ratio="1.7">
-      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-NInBEdSvp8"
-        title="Dubai Luxury Property Highlight The Glass Penthouse" frameborder="0"
+      <iframe
+        width="100%"
+        height="100%"
+        :src="video_link"
+        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </v-responsive>
-
   </v-container>
 </template>
 
-<script setup lang="ts">
-
+<script setup>
+const props = defineProps({
+  video_link: { type: String },
+});
 </script>
