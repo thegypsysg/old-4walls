8bd16a6d8406719064c785d992780d9dc1e41d9d
aijprasetyo471 <ajiprsty4713@gmail.com>
2024-10-23 22:19:19 +0700

    fix api 8 task 317

--- src/views/buy_detail/partials/video.vue
+++ src/views/buy_detail/partials/video.vue
@@ -8,7 +8,7 @@
       <iframe
         width="100%"
         height="100%"
-        :src="video_link"
+        :src="formattedVideoLink"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
@@ -19,7 +19,20 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
   video_link: { type: String },
+});
+
+const formattedVideoLink = computed(() => {
+  // Regex untuk menangkap ID video baik dari youtu.be atau youtube.com/watch?v=
+  const videoIdMatch = props.video_link?.match(
+    /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/|v\/|.+\/))([a-zA-Z0-9_-]{11})/,
+  );
+  const videoId = videoIdMatch ? videoIdMatch[1] : null;
+
+  // Gabungkan dengan base URL untuk embed jika videoId ditemukan
+  return videoId ? `https://www.youtube.com/embed/${videoId}` : null;
 });
 </script>
